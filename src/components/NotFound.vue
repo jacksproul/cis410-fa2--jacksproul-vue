58bf5949eeb9e035ec55f5097aaa5905f7d6b5d7
jacksproul <jack.sproul3@gmail.com>
2020-12-03 17:59:50 -0700

    13.2 11/19 add router and components

---
+++ src/components/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>NOT FOUND</h1>
+        <h4>Sorry, this page can't be found</h4>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/ReviewCreate.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>CREATE REVIEW</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
