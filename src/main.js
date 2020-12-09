7d54c198767a51c727468f116a956071f5d12a66
jacksproul <jack.sproul3@gmail.com>
2020-12-09 15:40:20 -0700

    15.1 12/1 Vuex store, sign in

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import axios from 'axios'
 
+import store from './store.js'
 import router from './routes.js'
 
 axios.defaults.baseURL = "https://cis410-fa20-jsproul3-api.azurewebsites.net"
 new Vue({
   el: '#app',
   router:router,
+  store: store,
   render: h => h(App)
 })
